707d1169c948c211a121a4ad9a6772c242650304
Dheeraj Kumar <dkchalotra@gmail.com>
2024-02-13 11:19:02 +0530

    tab item bottom border fix

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -9,8 +9,8 @@ function Container({ children, ...props }) {
 function Item({ title = "", icon: Icon, active = false, ...props }) {
   return (
     <li
-      className={`border-b-2 border-slate-${
-        active ? "600" : "300"
+      className={`border-b-2 ${
+        active ? "border-b-slate-600" : "border-b-slate-300"
       } hover:bg-slate-400 hover:text-slate-100 transition-colors`}
     >
       <button className="p-2 flex gap-2 items-center" {...props}>
